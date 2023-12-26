141deaaf6cc3b5a072734ceee3331fdaa170d5f8
Suhyugyeong <suhyugyeongsuh@gamil.com>
2023-12-26 13:55:36 +0900

    1226-react

---
+++ ch3/lab-vite-app/src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+//global css... css 파일을 직접 import, 모든 jsx 에서 다 이용
+import 'bootstrap/dist/css/bootstrap.css'
+
+import CSSTest from './lab1-style/Lab1_CSSTest.jsx'
+import StyledComponent from './lab2-styled-component/Lab2_StyledComponent.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <App /> */}
+    {/* <CSSTest /> */}
+    <StyledComponent />
+  </React.StrictMode>,
+)
