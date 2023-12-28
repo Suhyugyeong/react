c300ef4e150db00c8f723302e27bad14c61ad1b2
Suhyugyeong <suhyugyeongsuh@gamil.com>
2023-12-28 15:33:32 +0900

    1228

--- ch3/todolist-app/src/components/App.jsx
+++ ch3/todolist-app/src/components/App.jsx
@@ -11,10 +11,12 @@ const App = (props) => {
       <div className="card card-default card-borderless">
         <div className="card-body">
           <InputTodo addTodo={props.addTodo} />
+          {/* 할 일을 입력하는 컴포넌트인 InputTodo 컴포넌트를 렌더링하고, addTodo라는 콜백 함수를 InputTodo 컴포넌트에 전달합니다. */}
           <TodoList
             todoList={props.todoList}
             toggleDone={props.toggleDone}
             deleteTodo={props.deleteTodo}
+            // TodoList 컴포넌트를 렌더링하고, todoList, toggleDone, deleteTodo라는 세 가지 콜백 함수를 TodoList 컴포넌트에 전달합니다
           />
         </div>
       </div>
