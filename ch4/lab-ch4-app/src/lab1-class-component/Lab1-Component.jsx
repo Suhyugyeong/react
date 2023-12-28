f78109f273b00edc78ac431d0c956c63d70a43cf
Suhyugyeong <suhyugyeongsuh@gamil.com>
2023-12-28 10:38:13 +0900

    1228

--- ch4/lab-ch4-app/src/lab1-class-component/Lab1-Component.jsx
+++ ch4/lab-ch4-app/src/lab1-class-component/Lab1-Component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import TestComponent from "./Test1-component";
 import PropsComponent from "./Test2-props";
+import StateComponent from "./Test3-state";
 
 class ClassComponent extends React.Component {
   // Test1과 동일함 component를 import했느냐 차이
@@ -9,6 +10,7 @@ class ClassComponent extends React.Component {
       <div>
         <TestComponent />
         <PropsComponent />
+        <StateComponent />
       </div>
     );
   }
