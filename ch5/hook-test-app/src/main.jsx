eb5034a788712481e93a5b2a962c54e970d59a29
Suhyugyeong <suhyugyeongsuh@gamil.com>
2023-12-29 14:40:41 +0900

    1229

--- ch5/hook-test-app/src/main.jsx
+++ ch5/hook-test-app/src/main.jsx
@@ -4,12 +4,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 
-// import UseEffectComponent from "./lab1-useEffect/UseEffectTest";
+// import UseEffectComponent from './lab1-useEffect/UseEffectTest'
 import UseEffectComponent from "./lab2-useEffect-cleanup/UseEffectClock.";
+import ArrayReduceComponent from "./lab3-array-reduce/ArrayReduce";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <App /> */}
-    <UseEffectComponent />
+    {/* <UseEffectComponent /> */}
+    <ArrayReduceComponent />
   </React.StrictMode>
 );
