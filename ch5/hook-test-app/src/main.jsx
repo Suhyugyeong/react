3f0ced0802fc3aad0b3522ded4cc3d675c10d667
Suhyugyeong <suhyugyeongsuh@gamil.com>
2023-12-29 16:52:02 +0900

    1229

--- ch5/hook-test-app/src/main.jsx
+++ ch5/hook-test-app/src/main.jsx
@@ -7,13 +7,16 @@ import "bootstrap/dist/css/bootstrap.css";
 // import UseEffectComponent from './lab1-useEffect/UseEffectTest'
 // import UseEffectComponent from "./lab2-useEffect-cleanup/UseEffectClock";
 // import ArrayReduceComponent from "./lab3-array-reduce/ArrayReduce";
-import UseReducerComponent from "./lab4-useReducer/UseReducerTest";
+// import UseReducerComponent from "./lab4-useReducer/UseReducerTest";
+//여기 위치가 좀 다를거임
+import UseRefComponent from "./lab5-UseRef/UseRef";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <App /> */}
     {/* <UseEffectComponent /> */}
     {/* <ArrayReduceComponent /> */}
-    <UseReducerComponent />
+    {/* <UseReducerComponent /> */}
+    <UseRefComponent />
   </React.StrictMode>
 );
