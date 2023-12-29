161987e989ef595f8654476790d7bea8ea6d0de7
Suhyugyeong <suhyugyeongsuh@gamil.com>
2023-12-29 13:33:43 +0900

    1229

--- ch5/hook-test-app/src/lab2-useEffect-cleanup/Clock.jsx
+++ ch5/hook-test-app/src/lab2-useEffect-cleanup/Clock.jsx
@@ -4,6 +4,21 @@ import DateAndTime from "date-and-time";
 const Clock = (props) => {
   const [currentTime, setCurrentTime] = useState(new Date());
 
+  //setInterval에 의해 업무 진행(시간변경)이 최초 한번만 진행되게끔 해야함.. => 종속성을 표현하자!
+  //unmount 시에 inerval이 움직이지 않게..
+  useEffect(() => {
+    const handle = setInterval(() => {
+      console.log("##clock...");
+      setCurrentTime(new Date());
+    }, 1000); //console에 동일 로그를 계속 찍는 문제
+    //useEffect() 매개변수에 지정한 함수에서 리턴으로 함수를 리턴시키면..
+    //그 함수는 unmount 시에 호출해준다
+    //cleanup 함수라고 칭함
+    return () => {
+      clearInterval(handle);
+    };
+  }, []);
+
   return (
     <div>
       <h3>{DateAndTime.format(currentTime, props.formatString)}</h3>
--- ch5/hook-test-app/src/lab2-useEffect-cleanup/UseEffectClock..jsx
+++ ch5/hook-test-app/src/lab2-useEffect-cleanup/UseEffectClock..jsx
@@ -1,5 +1,19 @@
 import React, { useState } from "react";
 import Clock from "./Clock";
 
-const UseEffectComponent = () => {};
+//ch1에서는 class로 만들었음(값을 바꿀땐 setState) 여기서는 함수로! 둘의 차이 구분해봐요
+const UseEffectComponent = () => {
+  const [formatString, setFormatString] = useState("HH:mm:ss");
+  //HH를 사용하면 시간이 24시간 형식으로 표시되고, hh를 사용하면 12시간 형식으로 표시됩니다.
+  const [clockVisible, setClockVisible] = useState(false);
+  return (
+    <div>
+      <h2>시계</h2>
+      <button onClick={() => setClockVisible(!clockVisible)}>toggle</button>
+      <hr />
+      {clockVisible ? <Clock formatString={formatString} /> : ""}
+      {/* 삼항연산자가 조건문인건 맞지만, 보간법을 이용해서 결과가 나오기 때문에 가능한 */}
+    </div>
+  );
+};
 export default UseEffectComponent;
