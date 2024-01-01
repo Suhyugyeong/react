e28b02a34290095008774ac42a74595a391433a3
Suhyugyeong <suhyugyeongsuh@gamil.com>
2024-01-01 20:33:09 +0900

    0101

--- ch3/lab-vite-app/src/lab5-container-component/ContainerComponent.jsx
+++ ch3/lab-vite-app/src/lab5-container-component/ContainerComponent.jsx
@@ -4,7 +4,7 @@ import PresentationalComponent2 from "./PresentationalComponent2";
 
 //container component
 //화면을 목적으로 하지 않는다.
-//공통의 데이터를 이용하는 자식 컴포넌트의 부모 역할이 주 목적이다.
+//공통의 데이터를 이용하는 자식 컴포넌트의 부모 역할이 주목적이다.
 //자식들의 공통 데이터와 그 데이터를 위한 업무가 주 목적..
 const ContainerComponent = () => {
   const [count, setCount] = useState(0);
--- ch4/todolist-app-class/src/components/AppContainer.jsx
+++ ch4/todolist-app-class/src/components/AppContainer.jsx
@@ -15,6 +15,7 @@ class AppContainer extends React.Component {
 
   addTodo = (todo) => {
     let newTodoList = produce(this.state.todoList, (draft) => {
+      //immer 라이브러리의 produce 함수를 사용하여 불변성을 유지하면서 새로운 배열을 생성
       draft.push({ no: new Date().getTime(), todo: todo, done: false });
     });
     // setTodoList(newTodoList);
@@ -24,8 +25,12 @@ class AppContainer extends React.Component {
 
   deleteTodo = (no) => {
     let index = this.state.todoList.findIndex((todo) => todo.no === no);
+    // findIndex는 JavaScript 언어에서 기본으로 제공되는 배열 메서드 중 하나
     let newTodoList = produce(this.state.todoList, (draft) => {
+      //draft는 원본 배열(this.state.todoList)의 변경을 허용하는 임시적인 작업 영역입니다. produce 함수는 이 작업 영역에서 변경된 내용을 추적하고 적절한 불변성을 유지한 새로운 배열을 생성합니다.
+      //produce랑 draft의 개념이 아직 잘...
       draft.splice(index, 1);
+      //splice 메서드를 사용하여 배열에서 특정 인덱스(index)의 요소를 1개 제거합니다.
     });
     // setTodoList(newTodoList);
     this.setState({ todoList: newTodoList });
