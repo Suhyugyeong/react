d20ccd69b86178c3800ce760b9a5a201965b15f0
Suhyugyeong <suhyugyeongsuh@gamil.com>
2024-01-04 15:45:39 +0900

    0104

--- ch8/router-test-app/src/lab5-nested-route/About.jsx
+++ ch8/router-test-app/src/lab5-nested-route/About.jsx
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSearchParams, useNavigate } from "react-router-dom";
+
+//url의 query문자열(data)을 활용해서 페이징 처리를 해볼거임
+//현재 페이지가 몇 페이지인지, 데이터 유지가 되어야 하고
+//next, prev 버튼을 클릭했을 때 몇 페이지를 원하는지 정보가 전달되어야 한다
+//이것을 query 문자열로 해보겠음
 
 const About = (props) => {
+  let [searchParams, setSearchParams] = useSearchParams();
+  const [page, setPage] = useState(1); //화면의 페이지 정보
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    //searchParams가 변경되었다는 것은 url의 search 문자열이 변경되었다는 것
+    //search문자열에 page 정보가 있다. search에서 새로운 페이지 정보를 획득해서
+    //state조정
+    const strPage = searchParams.get("page"); //~~~~?page=2
+    setPage(parseInt(strPage !== null ? strPage : "1", 10));
+  }, [searchParams]);
+
+  const goPrev = () => {
+    //이전 페이지 버튼 클릭
+    //search 문자열을 조정해서 페이지 이동하게
+    if (page > 1) navigate(location.pathname + "?page=" + (page - 1));
+  };
+  const goNext = () => {
+    navigate(location.pathname + "?page=" + (page + 1));
+  };
+
   return (
     <div className="card card-body">
       <h2>About {props.title}</h2>
+      <div>
+        <div className="m-2">현재 페이지: {page}</div>
+        <button className="btn btn-secondary m-1" onClick={goPrev}>
+          prev
+        </button>
+        <button className="btn btn-secondary m-1" onClick={goNext}>
+          next
+        </button>
+      </div>
     </div>
   );
 };
--- ch8/router-test-app/src/lab5-nested-route/SongList.jsx
+++ ch8/router-test-app/src/lab5-nested-route/SongList.jsx
@@ -1,10 +1,24 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useMatch } from "react-router-dom";
 
 const SongList = (props) => {
+  //useMatch() - 매개변수에 path 패턴을 지정하고 이 패턴에 만족해서 실행된 것인지를 판단
+  //만족하면 path 정보를 넘겨줌
+  //주로 outer component가 inner component가 실행될 때 패턴인지를 파악해서 구체적인 정보를 뽑아서..
+  //다양한 UI적 처리를 할 때 이용함
+
+  const pathMatch = useMatch("/songs/:id"); //자기 밑에 뭔가 하위까지 같이 나오는거임
+  let param_id = -1;
+  if (pathMatch) {
+    //이걸 쓰는 이유는 /songs혹은 /songs/1일 경우도 있기 때문에
+    param_id = parseInt(pathMatch.params.id, 10);
+  }
+
   let list = props.songs.map((song) => {
+    let cn = "list-group-item";
+    cn += param_id === song.id ? " list-group-item-secondary" : "";
     return (
-      <li className="list-group-item" key={song.id}>
+      <li className={cn} key={song.id}>
         <Link to={`/songs/${song.id}`} style={{ textDecoration: "none" }}>
           {song.title} ({song.musician})
           <span className="float-end badge bg-secondary">
